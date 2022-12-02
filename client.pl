dbd1ff045c20f6eeba4fa27b1de87b0f1062ad67
raju-tati <mesg.raj@outlook.com>
2022-12-02 14:47:04 +0530

    third commit

--- client.pl
+++ client.pl
@@ -1,8 +1,14 @@
 use strict;
 use warnings;
 use utf8;
+use POSIX;
+use Digest::SHA1 qw(sha1);
+use Encode;
+use Bencode qw(bencode bdecode);
+use LWP::Simple qw(get);
+use Try::Tiny;
 
-sub file_content() {
+sub file_content {
     my ($file) = @_;
     my $contents;
     open( my $fh, '<', $file ) or die "Cannot open torrent $file";
@@ -102,9 +108,55 @@ sub _dechunk {
     return $chunks;
 }
 
-
-sub main() {
-    my $torrent_file = 'ubuntu-22.04.1-live-server-amd64.iso.torrent';
-    my $torrent      = bdecode( file_content($torrent_file) );
-
+sub get_infoHash {
+    my ($infoKey) = @_;
+    my $info_hash = Encode::encode( "ISO-8859-1", sha1( bencode( $infoKey )));
+    return $info_hash;
 }
+
+sub main {
+    my ($torrent_file) = @_;
+    my $fileContent    = file_content($torrent_file);
+    my $torrent        = bdecode( $fileContent );
+
+    my $file_name    = $torrent->{'info'}->{'name'};
+    my $file_length  = $torrent->{'info'}->{'length'};
+    my $piece_length = $torrent->{'info'}->{'piece length'};
+
+    my $infoKey = $torrent->{'info'};
+    my $info_hash = get_infoHash($infoKey);
+
+    my $announce   = $torrent->{'announce'};
+    my $port       = 6881;
+    my $left       = $torrent->{'info'}->{'length'};
+    my $uploaded   = 0;
+    my $downloaded = 0;
+    my $peer_id    = "-AZ2200-6wfG2wk6wWLc";
+
+    # $tracker request from torrent file info
+    my $tracker_request =
+        $announce
+      . "?info_hash="
+      . $info_hash
+      . "&peer_id="
+      . $peer_id
+      . "&port="
+      . $port
+      . "&uploaded="
+      . $uploaded
+      . "&downloaded="
+      . $downloaded
+      . "&left="
+      . $left;
+
+
+
+
+    use Data::Dumper;
+    print Dumper $torrent, "\n";
+    print Dumper $tracker_request, "\n";
+}
+
+my $torrent_file = '/home/pc/Documents/GIT/TorrentClient/ubuntu-22.04.1-live-server-amd64.iso.torrent';
+#my $torrent_file = "archlinux-2022.12.01-x86_64.iso.torrent";
+main($torrent_file);
