c6d92c9c9c01f981ec6749b0a7380f9dd191c8ab
raju-tati <118584250+raju-tati@users.noreply.github.com>
2023-02-26 10:42:28 +0530

    Update client.pl

--- client.pl
+++ client.pl
@@ -1,6 +1,5 @@
 use strict;
 use warnings;
-use utf8;
 use POSIX;
 use Digest::SHA1 qw(sha1);
 use Encode;
@@ -20,99 +19,12 @@ sub file_content {
     return $contents;
 }
 
-sub bencode {
-    no locale;
-    my $item = shift;
-    my $line = '';
-    if(ref($item) eq 'HASH') {
-        $line = 'd';
-        foreach my $key (sort(keys %{$item})) {
-            $line .= bencode($key);
-            $line .= bencode(${$item}{$key});
-        }
-        $line .= 'e';
-        return $line;
-    }
-    if(ref($item) eq 'ARRAY') {
-        $line = 'l';
-        foreach my $l (@{$item}) {
-            $line .= bencode($l);
-        }
-        $line .= 'e';
-        return $line;
-    }
-    if($item =~ /^\d+$/) {
-        $line = 'i';
-        $line .= $item;
-        $line .= 'e';
-        return $line;
-    }
-    $line = length($item).":";
-    $line .= $item;
-    return $line;
-}
-
-sub bdecode {
-    my $string = shift;
-    my @chunks = split(//, $string);
-    my $root = _dechunk(\@chunks);
-    return $root;
-}
-
-sub _dechunk {
-    my $chunks = shift;
-
-    my $item = shift(@{$chunks});
-    if($item eq 'd') {
-        $item = shift(@{$chunks});
-        my %hash;
-        while($item ne 'e') {
-            unshift(@{$chunks}, $item);
-            my $key = _dechunk($chunks);
-            $hash{$key} = _dechunk($chunks);
-            $item = shift(@{$chunks});
-        }
-            return \%hash;
-    }
-    if($item eq 'l') {
-        $item = shift(@{$chunks});
-        my @list;
-        while($item ne 'e') {
-            unshift(@{$chunks}, $item);
-            push(@list, _dechunk($chunks));
-            $item = shift(@{$chunks});
-        }
-        return \@list;
-    }
-    if($item eq 'i') {
-        my $num;
-        $item = shift(@{$chunks});
-        while($item ne 'e') {
-            $num .= $item;
-            $item = shift(@{$chunks});
-        }
-        return $num;
-    }
-    if($item =~ /\d/) {
-        my $num;
-        while($item =~ /\d/) {
-            $num .= $item;
-            $item = shift(@{$chunks});
-        }
-        my $line = '';
-        for(1 .. $num) {
-            $line .= shift(@{$chunks});
-        }
-        return $line;
-    }
-    return $chunks;
-}
-
 sub get_infoHash {
     my ($infoKey) = @_;
     my $info_hash = Encode::encode( "ISO-8859-1", sha1( bencode( $infoKey )));
     return $info_hash;
 }
+
 
 sub main {
     my ($torrent_file) = @_;
@@ -150,13 +62,9 @@ sub main {
       . $left;
 
 
-
-
-    use Data::Dumper;
-    print Dumper $torrent, "\n";
-    print Dumper $tracker_request, "\n";
+    use Data::Printer;
+    p $tracker_request;
 }
 
-my $torrent_file = '/home/pc/Documents/GIT/TorrentClient/ubuntu-22.04.1-live-server-amd64.iso.torrent';
-#my $torrent_file = "archlinux-2022.12.01-x86_64.iso.torrent";
+my $torrent_file = 'debian-11.6.0-amd64-netinst.iso.torrent';
 main($torrent_file);
