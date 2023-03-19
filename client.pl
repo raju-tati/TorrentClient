4843c8198cedf907ccb635d99f0df8198d767b61
raju-tati <118584250+raju-tati@users.noreply.github.com>
2023-03-19 19:19:37 +0530

    Update client.pl

--- client.pl
+++ client.pl
@@ -26,6 +26,13 @@ sub file_content {
     return $contents;
 }
 
+sub save_piece {
+    my ($content, $name) = @_;
+    my $fh = aio_open "pieces/$name", O_WRONLY | O_TRUNC | O_CREAT, 0666 or warn "Error: $!";
+    aio_write $fh, 0, length($content), $content, 0 or warn "aio_write: $!";
+    aio_close $fh;
+}
+
 sub get_infoHash {
     my ($infoKey) = @_;
     my $info_hash = Encode::encode( "ISO-8859-1", sha1( bencode( $infoKey )));
@@ -82,9 +89,9 @@ sub main {
         $piece_channel->put($n);
     }
 
-    tcp_connect $peers->[1]->{'ip'}, $peers->[1]->{'port'}, Coro::rouse_cb;
+    tcp_connect $peers->[0]->{'ip'}, $peers->[0]->{'port'}, Coro::rouse_cb;
     my $fh = unblock +(Coro::rouse_wait)[0];
-    
+
     my $pstr = "BitTorrent protocol";
     my $message = pack 'C1A*a8a20a20', length($pstr), $pstr, '',  $info_hash, $peer_id;    
     
@@ -110,7 +117,66 @@ sub main {
 
         my ($length, $id) = unpack 'Nc', $choke_buf;
         if( $id == 1 ) {
-            # got unchoke, code goes here
+            PIECELOOP: {
+                my $block_length = 2 ** 14;
+
+                if( $piece_channel->size == 0 ) {
+                    terminate;
+                }
+                my $piece_index = $piece_channel->get;
+
+                #if(defined($bitfield_array[$piece_index]) && $bitfield_array[$piece_index] == 1) {
+                    my $piece_data = '';
+                    my $piece_offset = 0;
+
+                    BLOCKLOOP: {
+                        my $block_buf;
+                        my $block_buf_size = 4 + 1 + 4 + 4 + $block_length;
+
+                        if( $piece_index == $bitfields_num - 1 ) {
+                            # handle last piece
+                            # $bitfields_num = number of pieces
+
+                            my $extra = ($bitfields_num * $piece_length) - $file_length;
+                            my $last_piece_length = $piece_length - $extra;
+
+                            if ( $piece_offset == $last_piece_length ) {
+                                save_piece($piece_data, $piece_index);
+                                goto PIECELOOP;
+                            }
+                        }
+
+                        if( $piece_offset == $piece_length ) {
+                            save_piece($piece_data, $piece_index);
+                            goto PIECELOOP;
+                        }
+
+                        my $request_pack = pack 'NNN', $piece_index, $piece_offset, $block_length;
+                        my $request = pack 'Nca*', length($request_pack) + 1, 6, $request_pack;
+
+                        $fh->syswrite($request);
+                        $fh->sysread($block_buf, $block_buf_size);
+
+                        my ($r_block_length, $r_block_id, $r_block_pack) = unpack 'Nca*', $block_buf;
+                        my $r_block_data_length = 16384; #($r_block_length - 9);
+                        my $unpack = 'N N'. ' a' . $r_block_data_length;
+                        my ($r_block_index, $r_block_offset, $r_block_data) = unpack $unpack, $r_block_pack;
+
+                        $piece_data = $piece_data . $r_block_data;
+                        $piece_offset = $piece_offset + $block_length;
+
+                            # ...
+                        goto BLOCKLOOP;
+                    } 
+                #}
+                #else {
+                    # put back piece_index on piece channel
+                    # let other workers download it
+                #    Coro::AnyEvent::sleep 1;
+                #    $piece_channel->put($piece_index);
+                 #   goto PIECELOOP;
+                #}
+            }
         } 
         elsif( $id == 0 ) {
             # got choke
@@ -121,3 +187,6 @@ sub main {
 
 my $torrent_file = 'debian-11.6.0-amd64-netinst.iso.torrent';
 main($torrent_file);
+
+print("At loop\n");
+EV::loop();
